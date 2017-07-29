e837c23dd4066031abc1c7067e53ca82e92142c1
Webbeeshive <webbeeshive.team@gmail.com>
2017-07-29 17:15:58 +0530

    Port updated

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function AppBegin(){
-    const port = 4500;
+    const port = 3000;
     const express = require("express");
     const app = express();
 
     app.use(express.static('ui'));
 
-    app.listen(port,function(){
+    app.listen(process.env.PORT || port,function(){
         console.log('Application started');
     });
 }
