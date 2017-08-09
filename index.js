6b1212c1a1c36cdb3c98d31fe628cfbdfe14bb27
esutima <sutirtha.marjit@ericsson.com>
2017-08-09 14:19:04 +0530

    RootCause : Debug : 003

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ function AppBegin(){
     const port = 3000;
     const path = require('path');
     const express = require("express");
-    //const CustomJobs = require('./customjobs');
+    const CustomJobs = require('./customjobs');
     console.log('Dependencies loaded into environment');
     const app = express();
     console.log('Express app created');
-    //var customjobs = new CustomJobs();
-    //customjobs.appIndexFileModifier('SerisPhotoDownloader');
+    var customjobs = new CustomJobs();
+    customjobs.appIndexFileModifier('SerisPhotoDownloader');
+    console.log('SerisPhotoDownloader configured');
     app.use(express.static('ui'));    
     console.log('Static folder assigned');
-    //app.use('/SerisPhotoDownloader', express.static('apps/SerisPhotoDownloader/dist/'));
+    app.use('/SerisPhotoDownloader', express.static('apps/SerisPhotoDownloader/dist/'));
 
     console.log('Ready to listen');
     app.listen(process.env.PORT || port,function(){
