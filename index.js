1f27af535885c9afe32d215a6af756dfcda42363
esutima <sutirtha.marjit@ericsson.com>
2017-08-09 14:51:05 +0530

    RootCause : Debug : 005

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function AppBegin(){
     const port = 3000;
     const path = require('path');
     const express = require("express");
-    const CustomJobs = require('./customjobs');
+    //const CustomJobs = require('./customjobs');
     console.log('Dependencies loaded into environment');
     const app = express();
     console.log('Express app created');
