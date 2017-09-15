1db2e57b15e38665c5baa4a049fe6bb617210d95
esutima <sutirtha.marjit@ericsson.com>
2017-09-15 15:03:22 +0530

    newcli-test

--- remove-packages.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var testFolder = "./node_modules";
-
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log('npm uninstall '+file);
-  });
-})
